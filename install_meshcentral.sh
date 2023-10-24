f97335ecdcc512408973afc3d550c58f84ff5ad8
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2023-10-24 16:46:00 +0700

    Update install_meshcentral.sh

--- install_meshcentral.sh
+++ install_meshcentral.sh
@@ -45,250 +45,23 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
 sudo apt install php8.0-fpm php8.0-common php8.0-mbstring php8.0-xmlrpc php8.0-soap php8.0-gd php8.0-xml php8.0-intl php8.0-mysql php8.0-cli php8.0-mcrypt php8.0-ldap php8.0-zip php8.0-curl php8.0-bz2 -y
 
-#Step 3. Cấu hình php.ini
-#Open PHP-FPM config file.
-#sudo nano /etc/php/8.0/fpm/php.ini
-#Add/Update the values as shown. You may change it as per your requirement.
-# if new php.ini configure then clear sign sharp # comment
-cat > /etc/php/8.0/fpm/php.ini <<END
-[PHP]
-engine = On
-short_open_tag = Off
-precision = 14
-output_buffering = 4096
-zlib.output_compression = Off
-implicit_flush = Off
-unserialize_callback_func =
-serialize_precision = -1
-disable_functions = 
-disable_classes =
-zend.enable_gc = On
-zend.exception_ignore_args = On
-zend.exception_string_param_max_len = 0
-expose_php = Off
-file_uploads = On 
-allow_url_fopen = On 
-memory_limit = 1200M 
-upload_max_filesize = 4096M
-max_execution_time = 360 
-cgi.fix_pathinfo = 0 
-date.timezone = asia/ho_chi_minh
-max_input_time = 60
-max_input_nesting_level = 64
-max_input_vars = 5000
-post_max_size = 4096M
-error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT
-display_errors = Off
-display_startup_errors = Off
-log_errors = On
-ignore_repeated_errors = Off
-ignore_repeated_source = Off
-report_memleaks = On
-variables_order = "GPCS"
-request_order = "GP"
-register_argc_argv = Off
-auto_globals_jit = On
-auto_prepend_file =
-auto_append_file =
-default_mimetype = "text/html"
-default_charset = "UTF-8"
-doc_root =
-user_dir =
-enable_dl = Off
-allow_url_fopen = On
-allow_url_include = Off
-default_socket_timeout = 60
-extension=bz2
-extension=curl
-;extension=ffi
-;extension=ftp
-extension=fileinfo
-;extension=gd
-;extension=gettext
-;extension=gmp
-extension=intl
-;extension=imap
-;extension=ldap
-extension=mbstring
-;extension=exif      ; Must be after mbstring as it depends on it
-;extension=mysqli
-;extension=oci8_12c  ; Use with Oracle Database 12c Instant Client
-;extension=oci8_19  ; Use with Oracle Database 19 Instant Client
-;extension=odbc
-extension=openssl
-;extension=pdo_firebird
-;extension=pdo_mysql
-;extension=pdo_oci
-;extension=pdo_odbc
-;extension=pdo_pgsql
-;extension=pdo_sqlite
-;extension=pgsql
-;extension=shmop
-;extension=snmp
-;extension=soap
-;extension=sockets
-;extension=sodium
-;extension=sqlite3
-;extension=tidy
-;extension=xsl
-;zend_extension=opcache
-[CLI Server]
-cli_server.color = On
-
-[Date]
-;date.timezone =
-
-; https://php.net/date.default-latitude
-;date.default_latitude = 31.7667
-
-; https://php.net/date.default-longitude
-;date.default_longitude = 35.2333
-
-
-[filter]
-
-[iconv]
-
-[imap]
-
-[intl]
-
-[sqlite3]
-
-[Pcre]
-
-[Pdo]
-
-[Pdo_mysql]
-pdo_mysql.default_socket=
-
-[Phar]
-
-[mail function]
-SMTP = localhost
-; https://php.net/smtp-port
-smtp_port = 25
-;sendmail_from = me@example.com
-;sendmail_path =
-;mail.force_extra_parameters =
-mail.add_x_header = Off
-;mail.log = syslog
-
-[ODBC]
-odbc.allow_persistent = On
-odbc.check_persistent = On
-odbc.max_persistent = -1
-odbc.max_links = -1
-odbc.defaultlrl = 4096
-odbc.defaultbinmode = 1
-
-[MySQLi]
-mysqli.max_persistent = -1
-mysqli.allow_persistent = On
-mysqli.max_links = -1
-mysqli.default_port = 3306
-mysqli.default_socket =
-mysqli.default_host =
-mysqli.default_user =
-mysqli.default_pw =
-mysqli.reconnect = Off
-
-[mysqlnd]
-mysqlnd.collect_statistics = On
-mysqlnd.collect_memory_statistics = Off
-
-[OCI8]
-
-[PostgreSQL]
-pgsql.allow_persistent = On
-pgsql.auto_reset_persistent = Off
-pgsql.max_persistent = -1
-pgsql.max_links = -1
-pgsql.ignore_notice = 0
-pgsql.log_notice = 0
-
-[bcmath]
-bcmath.scale = 0
-
-[browscap]
-
-[Session]
-session.save_handler = files
-session.use_strict_mode = 0
-session.use_cookies = 1
-session.use_only_cookies = 1
-session.name = PHPSESSID
-session.auto_start = 0
-session.cookie_lifetime = 0
-session.cookie_path = /
-session.cookie_domain =
-session.cookie_httponly = on
-session.cookie_samesite =
-session.serialize_handler = php
-session.gc_probability = 0
-session.gc_divisor = 1000
-session.gc_maxlifetime = 1440
-session.referer_check =
-session.cache_limiter = nocache
-session.cache_expire = 180
-session.use_trans_sid = 0
-session.sid_length = 26
-session.trans_sid_tags = "a=href,area=href,frame=src,form="
-session.sid_bits_per_character = 5
-
-[Assertion]
-zend.assertions = -1
-
-[COM]
-
-[mbstring]
-
-[gd]
-
-[exif]
-
-[Tidy]
-tidy.clean_output = Off
-
-[soap]
-soap.wsdl_cache_enabled=1
-soap.wsdl_cache_dir="/tmp"
-soap.wsdl_cache_ttl=86400
-soap.wsdl_cache_limit = 5
-
-[sysvshm]
-
-[ldap]
-ldap.max_links = -1
-
-[dba]
-
-[opcache]
-
-[curl]
-
-[openssl]
-
-[ffi]
-END
-
 systemctl restart php8.0-fpm.service
 
-#Step 4. Cài đặt MariaDB
+#Step 3. Cài đặt MariaDB
 sudo apt install mariadb-server mariadb-client -y
 sudo systemctl stop mysql.service 
 sudo systemctl start mysql.service 
 sudo systemctl enable mysql.service
 
-#Step 5. Cài đặt phpMyAdmin
+#Step 4. Cài đặt phpMyAdmin
 sudo apt install phpmyadmin -y
 
-#Step 6. # Cấu hình Nginx để chạy MeshCentral
+#Step 5. # Cấu hình Nginx để chạy MeshCentral
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
 # xoa cau hinh nginx site default
-sudo rm -rf /etc/nginx/conf.d/$FQDN.conf
+#sudo rm -rf /etc/nginx/conf.d/$FQDN.conf
 
 #Step 7. Cấu hình MariaDB
 #Run the following command to secure MariaDB installation.
@@ -312,50 +85,35 @@ mysql -uroot -prootpassword -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'
 mysql -uroot -prootpassword -e "FLUSH PRIVILEGES";
 #mysql -uroot -prootpassword -e "SHOW DATABASES";
 
-# Nếu đã có thì bỏ qua đoạn hàm này như thế nào ?
-#Step 9. Next, edit the MariaDB default configuration file and define the innodb_file_format:
-#nano /etc/mysql/mariadb.conf.d/50-server.cnf
-#Add the following lines inside the [mysqld] section: 
-# if new php.ini configure then clear sign sharp # comment
-#cat > /etc/mysql/mariadb.conf.d/50-server.cnf <<END
-#[mysqld]
-#innodb_file_format = Barracuda
-#innodb_file_per_table = 1
-#innodb_large_prefix = ON
-#END
-
 #Save the file then restart the MariaDB service to apply the changes.
 #systemctl restart mariadb
 
 #Step 10. Download và Cài đặt MeshCentral
-sudo mkdir /opt/$FQDN
-sudo chown -R $USER:$USER /opt/$FQDN
-cd /opt/$FQDN
 wget https://github.com/Ylianst/MeshCentral/archive/refs/tags/${GitMCversion}.zip
-unzip ${GitMCversion}.zip
 
 sudo mkdir /var/www
-sudo mkdir /var/www/$FQDN
+unzip ${GitMCversion}.zip -d /var/www/
+mv /var/www/MeshCentral-${GitMCversion}/ /var/www/$FQDN
+
 sudo chown -R $USER:$USER /var/www/$FQDN
 cd /var/www/$FQDN
-cp -r /opt/$FQDN/MeshCentral-${GitMCversion} /var/www/$FQDN/
 sudo chmod -R 755 /var/www/$FQDN
 sudo chown -R www-data:www-data /var/www/$FQDN/
 
-npm install
+apt install npm -y
 
 #Step 11: Finish MeshCentral installation
-cat > /etc/hosts <<END
-127.0.0.1 $FQDN
-127.0.0.1 localhost
+#cat > /etc/hosts <<END
+#127.0.0.1 $FQDN
+#127.0.0.1 localhost
 
 # The following lines are desirable for IPv6 capable hosts
-::1     ip6-localhost ip6-loopback
-fe00::0 ip6-localnet
-ff00::0 ip6-mcastprefix
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-END
+#::1     ip6-localhost ip6-loopback
+#fe00::0 ip6-localnet
+#ff00::0 ip6-mcastprefix
+#ff02::1 ip6-allnodes
+#ff02::2 ip6-allrouters
+#END
 
 rm ${GitMCversion}.zip
 # Tạo tệp cấu hình cho MeshCentral
